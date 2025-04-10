b344975ccf5bf55c663c6ab1bf93fcd4d630acb8
Lidiia-art <73796994+Lidiia-art@users.noreply.github.com>
2025-04-10 22:04:46 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const updatedState = [];
+  let newState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      newState = { ...newState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      newState = { ...newState };
+
+      for (const item of action.keysToRemove) {
+        delete newState[item];
+      }
+    }
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    updatedState.push(newState);
+  }
+
+  return updatedState;
 }
 
 module.exports = transformStateWithClones;
